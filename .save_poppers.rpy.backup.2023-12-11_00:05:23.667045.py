70aebf8f832172cc8ac1c13493227f3f1346f6a4
Ryan Burgert <rburgert@adobe.com>
2023-12-12 00:26:26 -0800

    save poppers

---
+++ .save_poppers.rpy.backup.2023-12-11_00:05:23.667045.py
@@ -0,0 +1,20 @@
+results=get_all_folders('/workspace/Diffusion-Illusions/new_borealis_ROTATOR_pop')
+slanko=[]
+def do_result(x):
+    try:
+        json=load_json(path_join(x,'metadata.json'))
+        seed=json.SEED
+        title=json.title
+        outname=str(seed)+' '+title+'.png'
+        in_name=get_all_image_files(x)
+        in_name=[x for x in in_name if 'logger' in x]
+        in_name=sorted(in_name)
+        in_name=in_name[-1]
+        image=load_image(in_name)
+        out='/workspace/poppers/'
+        out=path_join(out,outname)
+        save_image(image,outname)
+        print('Saved',outname)
+    except Exception as e:
+        fansi_print(e,'red')
+par_map(do_result,results)
